9490d5e1c8aeeea73aeba8d34ef0ea49330564b4
Jonny Cheetham <jcheetham@amplience.com>
2024-02-07 09:31:01 +0000

    feat: TAR-94 No need for the description to be configurable by the user

--- src/components/seoMetaTags/getDescription.ts
+++ src/components/seoMetaTags/getDescription.ts
@@ -3,7 +3,6 @@ import { getParams } from "../../lib/getParams";
 
 export const getDescription = (sdk: ContentFieldExtension) => {
   const { variant } = getParams(sdk);
-  const defaultDescription = `Generate an effective SEO ${variant} based on the content of the page.`;
 
-  return sdk.field.schema.description || defaultDescription;
+  return `Generate an effective SEO ${variant} based on the content of the page.`;
 };
