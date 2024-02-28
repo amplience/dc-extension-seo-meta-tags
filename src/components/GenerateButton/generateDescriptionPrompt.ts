1bea38092cfeaf57f27608c208252754ae7aa3c7
Jonny Cheetham <jcheetham@amplience.com>
2024-02-28 16:45:05 +0000

    feat: TAR-121 Description generation working

--- src/components/GenerateButton/generateDescriptionPrompt.ts
+++ src/components/GenerateButton/generateDescriptionPrompt.ts
@@ -1,16 +1,11 @@
 export const generateDescriptionPrompt = (content: string) => [
   {
-    role: "system",
+    role: "SYSTEM",
     content:
       "You are an SEO expert. You will summarize the text provided by the user for use as a SERP description. You will limit the description to around 120 characters. You can only respond with the description.",
   },
   {
-    role: "user",
+    role: "USER",
     content,
-  },
-  {
-    role: "system",
-    content:
-      'Format the response as a json object e.g. {"description": "the response"}',
   },
 ];
