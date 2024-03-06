ab084d20aa093f5dd2f5e2fe10ba7ba369f98aa6
Jonny Cheetham <jcheetham@amplience.com>
2024-03-06 10:27:04 +0000

    feat: TAR-121 Fixed typo in description prompt

--- src/components/GenerateButton/generateDescriptionPrompt.ts
+++ src/components/GenerateButton/generateDescriptionPrompt.ts
@@ -11,7 +11,7 @@ export const generateDescriptionPrompt = (content: string) => [
   },
   {
     role: "SYSTEM",
-    conent: `** Make sure to apply the following restrictions, follow them directly and don't skip any of them:
+    content: `** Make sure to apply the following restrictions, follow them directly and don't skip any of them:
     1. You will limit the description to around 120 characters
     2. You can only respond with the description
     3. Your response must not be conversational
