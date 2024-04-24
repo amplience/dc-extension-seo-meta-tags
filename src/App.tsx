06c3ad0cbf6d25f1026cb39d96ace64be7d26324
Chattox <33847899+Chattox@users.noreply.github.com>
2024-04-24 14:25:27 +0100

    feat: TAR-110 add datadog tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import { datadogRum } from "@datadog/browser-rum";
 import { SeoMetaTags } from "./components/SeoMetaTags/SeoMetaTags";
 import Theme from "./components/Theme";
 import { WithExtensionContext } from "./hooks/withExtensionContext";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const ddToken = import.meta.env.PUBLIC_DATADOG_TOKEN;
+    const ddId = import.meta.env.PUBLIC_DATADOG_ID;
+    const ddEnv = import.meta.env.PUBLIC_DATADOG_ENV;
+
+    if (ddToken && ddId && ddEnv) {
+      datadogRum.init({
+        applicationId: ddId,
+        clientToken: ddToken,
+        env: ddEnv,
+        site: "datadoghq.com",
+        service: "dc-extension-seo-meta-tags",
+        version: "1.1.0",
+        trackResources: true,
+        trackLongTasks: true,
+        trackUserInteractions: true,
+        defaultPrivacyLevel: "allow",
+        sessionSampleRate: 100,
+        usePartitionedCrossSiteSessionCookie: true,
+        proxy: "https://dd-proxy.amplience.net",
+      });
+
+      datadogRum.startSessionReplayRecording();
+    }
+  }, []);
+
   return (
     <WithExtensionContext>
       <Theme>
