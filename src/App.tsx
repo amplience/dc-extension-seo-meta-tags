d4a14ba1ad2c56b1a96f26040f8ed48d123881dd
Chattox <33847899+Chattox@users.noreply.github.com>
2024-04-25 11:08:20 +0100

    refactor: TAR-110 move initDatadog to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,8 @@
-import { datadogRum } from "@datadog/browser-rum";
 import { SeoMetaTags } from "./components/SeoMetaTags/SeoMetaTags";
 import Theme from "./components/Theme";
 import { WithExtensionContext } from "./hooks/withExtensionContext";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    const ddToken = import.meta.env.PUBLIC_DATADOG_TOKEN;
-    const ddId = import.meta.env.PUBLIC_DATADOG_ID;
-    const ddEnv = import.meta.env.PUBLIC_DATADOG_ENV;
-
-    if (ddToken && ddId && ddEnv) {
-      datadogRum.init({
-        applicationId: ddId,
-        clientToken: ddToken,
-        env: ddEnv,
-        site: "datadoghq.com",
-        service: "dc-extension-seo-meta-tags",
-        trackResources: true,
-        trackLongTasks: true,
-        trackUserInteractions: true,
-        defaultPrivacyLevel: "allow",
-        sessionSampleRate: 100,
-        sessionReplaySampleRate: 100,
-        usePartitionedCrossSiteSessionCookie: true,
-        proxy: "https://dd-proxy.amplience.net",
-      });
-
-      datadogRum.startSessionReplayRecording();
-    }
-  }, []);
-
   return (
     <WithExtensionContext>
       <Theme>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { init as initGainsight } from "./lib/gainsight";
+import { initDatadog } from "./lib/datadog";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -10,3 +11,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 );
 
 initGainsight(window, import.meta.env.PUBLIC_APTRINSIC_APP_ID);
+initDatadog();
